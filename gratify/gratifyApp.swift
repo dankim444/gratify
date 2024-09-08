5471bf2e564893f395e118e6e177ec21b7903ebd
Dan <dankim1@seas.upenn.edu>
2024-09-08 11:52:11 -0400

    Initial Commit

---
+++ gratify/gratifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gratifyApp.swift
+//  gratify
+//
+//  Created by Daniel Kim on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct gratifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
